a05a61a5d0ba3672e22e4ef1e686f44c5d96ea02
FINDarkside <findarkside@gmail.com>
2019-02-02 01:15:45 +0200

    Implement markdown in descriptions. Add preview of rendered markdown.

---
+++ src/components/general/MarkdownRenderer.vue
@@ -0,0 +1,70 @@
+<template>
+  <iframe ref="renderFrame"></iframe>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import MarkdownIt from 'markdown-it';
+
+const md = new MarkdownIt({
+  html: false,
+  xhtmlOut: false,
+  breaks: false,
+  langPrefix: 'language-',
+  linkify: false,
+  typographer: false,
+  quotes: '“”‘’',
+});
+
+@Component
+export default class MarkdownRenderer extends Vue {
+  // TODO: Fix requiring exclamation mark?
+  @Prop(String)
+  public markdown!: string;
+
+  @Watch('markdown')
+  markdownChanged() {
+    const markDowmHtml = md.render(this.markdown);
+    const pageHtml = `
+    <!doctype html><html>
+    <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+      <style>
+        .markdown-body{
+          font-family: 'Roboto';
+        }
+        html, body{
+          margin:0;
+          overflow: hidden;
+        }
+      </style>
+    </head>
+    <body class="markdown-body">${markDowmHtml}</body></html>`;
+    const frame = this.$refs.renderFrame as any;
+    frame.src = 'about:blank';
+    frame.contentWindow.document.open();
+    frame.contentWindow.document.write(pageHtml);
+    frame.contentWindow.document.close();
+    console.log(frame.contentWindow.document.body.offsetHeight);
+  }
+
+  resizeFrame() {
+    console.log('RESIZED');
+    const frame = this.$refs.renderFrame as any;
+    frame.style.height = frame.contentWindow.document.body.offsetHeight + 'px';
+  }
+
+  mounted() {
+  }
+}
+</script>
+
+<style>
+.sidebar-item {
+  text-decoration: none;
+  display: block;
+  padding: 10px;
+  widows: 100%;
+}
+</style>
--- src/components/pages/AddModPage.vue
+++ src/components/pages/AddModPage.vue
@@ -4,15 +4,36 @@
       <div class="display-2 mb-3 font-weight-medium mt-4">Add mod</div>
       <div class="add-mod-data-grid">
         <div class="title mod-info-title mt-2">Mod name</div>
-        <v-text-field solo label="Solo textarea" value="Relentless night"/>
+        <v-text-field :hide-details="true" solo label="Solo textarea" value="Relentless night"/>
 
         <div class="title mod-info-title mt-2">Mod description</div>
-        <v-textarea
-          :auto-grow="true"
-          solo
-          label="Solo textarea"
-          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-        ></v-textarea>
+        <v-card>
+          <v-tabs>
+            <v-tab ripple>Markdown</v-tab>
+            <v-tab ripple>Preview</v-tab>
+            <v-tab-item class="descriptionTabContent primary-scrollbar vertical">
+              <v-textarea
+                v-model="description"
+                :auto-grow="true"
+                :hide-details="true"
+                solo
+                flat
+                label="Solo textarea"
+                value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+              ></v-textarea>
+            </v-tab-item>
+            <v-tab-item
+              class="pa-3 descriptionTabContent primary-scrollbar vertical"
+              ref="descriptionTab"
+            >
+              <MarkdownRenderer
+                class="description-renderer description-preview-card"
+                :markdown="description"
+                ref="markdownRenderer"
+              />
+            </v-tab-item>
+          </v-tabs>
+        </v-card>
 
         <div class="title mod-info-title mt-2">Images</div>
         <div>
@@ -26,17 +47,36 @@
 </template>
 
 <script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import ImageDropArea from '@/components/general/ImageDropArea.vue';
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import MarkdownRenderer from '@/components/general/MarkdownRenderer.vue';
+
 import * as ModService from '@/networking/ModService';
 import store from '@/store';
 import { ImageData } from '@/components/general/ImageDropArea.vue';
 import { remote } from 'electron';
 
-@Component({ components: { ImageDropArea } })
+@Component({ components: { ImageDropArea, MarkdownRenderer } })
 export default class AddModPage extends Vue {
   name: string = '';
   images: ImageData[] = [];
+  description: string = '';
+  descriptionCompiled = '';
+
+  @Watch('description')
+  descriptionTabChanged(oldVal: string, newVal: string) {
+    // Do on next tick to allow markdownRenderer to compile the markdown before calculating height
+    Vue.nextTick(() => {
+      console.log({ oldVal, newVal });
+      if ((this.$refs.descriptionTab as any).$el.style.display === 'none') {
+        (this.$refs.descriptionTab as any).$el.style.display = 'block';
+        (this.$refs.markdownRenderer as any).resizeFrame();
+        (this.$refs.descriptionTab as any).$el.style.display = 'none';
+      } else {
+        (this.$refs.markdownRenderer as any).resizeFrame();
+      }
+    });
+  }
 
   addImagesClicked() {
     remote.dialog.showOpenDialog(
@@ -55,7 +95,7 @@ export default class AddModPage extends Vue {
 
 <style lang="stylus">
 #add-mod-page-content {
-  max-width: 900px;
+  max-width: 1020px;
   flex-grow: 1;
   margin: 0 auto;
   flex-grow: 1;
@@ -65,6 +105,16 @@ export default class AddModPage extends Vue {
     grid-template-rows: auto;
     grid-template-columns: auto 1fr;
     column-gap: 50px;
+    row-gap: 15px;
   }
+}
+
+.description-preview-card {
+  border: none;
+  width: 100%;
+}
+
+.descriptionTabContent {
+  max-height: 80vh;
 }
 </style>
