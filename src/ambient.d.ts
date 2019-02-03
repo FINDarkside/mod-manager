9c8c8aaaee2325caaa904418fd1207fa4a35f7e7
FINDarkside <findarkside@gmail.com>
2019-02-03 06:03:21 +0200

    Fix "cannot find module" warnings by tslint

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,1 +1,8 @@
-declare module 'vuedraggable';
+import Vue from 'vue';
+import { VuetifyObject } from 'vuetify';
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $vuetify: VuetifyObject;
+  }
+}
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,11 +3,4 @@ declare module '*.vue' {
   export default Vue;
 }
 
-import Vue from 'vue';
-import { VuetifyObject } from 'vuetify';
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $vuetify: VuetifyObject;
-  }
-}
+declare module 'vuedraggable';
