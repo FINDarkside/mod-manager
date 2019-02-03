7f79ee0ac86c577425b1b309574ad3c0df562f5b
FINDarkside <findarkside@gmail.com>
2019-02-03 06:54:36 +0200

    Add markdown renderer to mod page and make mod info card sticky

--- src/components/pages/ModPage.vue
+++ src/components/pages/ModPage.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class='page primary-scrollbar' id="mod-page">
+  <div class="page primary-scrollbar" id="mod-page">
     <div class="mod-page-grid">
       <div class="display-2 mb-2 font-weight-medium mx-auto mt-4 mb-2">Relentless Night</div>
       <div/>
-      <div class="mx-4">
-        <v-carousel class="mod-images-carousel mx-auto" height="324" :cycle="false" >
+      <div class="mx-4 pb-4">
+        <v-carousel class="mod-images-carousel mx-auto" height="324" :cycle="false">
           <v-carousel-item src="https://picsum.photos/576/324/?image=2" key="2"/>
           <v-carousel-item src="https://picsum.photos/1920/1080/?image=3" key="3"/>
           <v-carousel-item src="https://picsum.photos/576/324/?image=4" key="4"/>
@@ -14,16 +14,14 @@
           <v-expansion-panel-content>
             <div slot="header">
               <div class="flexbox vertical-align">
-                <v-icon>description</v-icon><span class="ml-3">Description</span>
+                <v-icon>description</v-icon>
+                <span class="ml-3">Description</span>
               </div>
             </div>
-            <v-card>
-              <v-card-text>
-                The earth seems to be slowing. Days and nights are getting longer. Every long night gets colder and harsher than the last. How long will you survive?
-                The Relentless Night mod is completely fan-made, I have no affiliation with Hinterland Studios.- DO NOT report bugs within the modded game to Hinterland Studios, it's possible bugs may be due to the mod code, please go to the Relentless Night Discord server to get help for any bugs you encounter.- The mod is experimental; There is no guarantee it wont corrupt any game saves, although a bug corrupting saves is extremely unlikely.
-              </v-card-text>
+            <v-card class="pa-3">
+              <MarkdownRenderer class="fullwidth" :markdown="mod.description"/>
             </v-card>
-            </v-expansion-panel-content>
+          </v-expansion-panel-content>
         </v-expansion-panel>
       </div>
       <div class="mr-4">
@@ -55,12 +53,17 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import MarkdownRenderer from '@/components/general/MarkdownRenderer.vue';
 import store from '@/store';
 
-@Component
+@Component({ components: { MarkdownRenderer } })
 export default class ModPage extends Vue {
   @Prop(String)
   modID!: string;
+
+  mod = {
+    description: "# Relentless Night\n\n**DISCLAIMER**\n\nThe Relentless Night mod is completely fan-made, I have no affiliation with Hinterland Studios.- DO NOT report bugs within the modded game to Hinterland Studios, it's possible bugs may be due to the mod code, please go to the [Relentless Night Discord server](https://discord.gg/9ndTkCF) to get help for any bugs you encounter.- The mod is experimental; There is no guarantee it wont corrupt any game saves, although a bug corrupting saves is extremely unlikely.\n\n\n## NEW V3.01 CHANGES\n* Updated all mod features to work with new Vigilant Flame update.\n* Coals can now be added to a fire without a waiting period (late-game balancing).\n* More fuel can be added to a fire even if the fire has reached its maximum duration, provided that the fuel can still increase the fire's current temperature. Although the duration of fire will not increase further, the heat bonus will still be applied. (late-game balancing)\n* The immediate area where an outdoor fire burned will also retain some heat a while longer after the fire's out, although of course the temperature bonus will drop and disappear much quicker compared to the heat from a fire inside an insulated building.\n* Fixed mod bug where failing to start a fire indoors could reset the residual heat bonus remaining inside back to zero in RN's heat retention feature.\n* Fixed mod bug where blizzard temperature drop didn't change temperature during blizzards in certain Relentless Night runs.\n* Fixed mod bug where the temperature of outdoors caves would sometimes get calculated incorrectly and were warmer than intended.\n* Fixed mod bug where custom mode option \"Day Length Multiplier\" would cause timing issues in RN runs if it was set above 1x. All values of this option can now be chosen for RN runs as well.\n* Should you need to correct something in the RN settings later into your game, you can now change specific setting values through new console commands. Loading the save you want to change and entering \"rn_help\" into the console will provide information on all RN commands needed to change each setting."
+  };
 
   expandState = [true];
 }
@@ -74,7 +77,10 @@ export default class ModPage extends Vue {
   flex-grow: 1;
 }
 
-.mod-info-card {
+#mod-page .mod-info-card {
+  position: sticky;
+  top: 10px;
+
   .info-title {
     text-transform: uppercase;
     font-size: 12px;
